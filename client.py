71a3881cbb3162cd0f9f9b51ae6341317a34dc8c
OppaCoding <adun221@naver.com>
2017-05-29 17:33:51 +0900

    글 읽

--- client.py
+++ client.py
@@ -1,7 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import tistoryApi
-
+import getpass
 
 secretKey = ""
 
@@ -10,7 +10,7 @@ tistory = tistoryApi.Tistory(client_id="d420680d548dc8d2c63b91010c017ecd", clien
 
 while tistory.state != "login":
     blogId = input("아이디를 입력해주세요: ")
-    blogPw = input("비밀번호를 입력해주세요:")
+    blogPw = input("비밀번호를 입력해주세요: ")
     tistory.login(id=blogId, pw=blogPw)
 
 print("로그인 성공")
@@ -20,6 +20,8 @@ while 1:
     print("1. 블로그 정보")
     print("2. 카테고리 목록")
     print("3. 글 목록")
+    print("4, 글 읽기")
+    print("5. 방명록")
     print("----------------------------")
 
     command = int(input("명령어를 선택해주세요: "))
@@ -31,14 +33,27 @@ while 1:
         print(tistory.categoryList())
 
     elif command is 3:
-        data = tistory.post()
+        data = tistory.post_list()
         count = data["totalCount"]
         posts = data["posts"]
 
         print("총 게시물 개수:", count)
 
         for post in posts:
-            print(post["title"])
+            print("ID:", post["id"], "제목:", post["title"])
+
+    elif command is 4:
+        id = int(input("읽을 글의 ID를 입력해주세요: "))
+
+        post = tistory.post_read(id)
+
+        print("글 제목 : ", post["title"])
+        print("글 등록 일 : ", post["date"])
+        print("글 내용 : ")
+        print(post["content"])
+
+    elif command is 5:
+        print(tistory.guestBook_list())
 
 
 
--- tistoryApi.py
+++ tistoryApi.py
@@ -12,94 +12,6 @@ driver = webdriver.Chrome(executable_path=chromeDriver)
 
 class Tistory:
 
-
-    def __init__(self, client_id, redirect_uri, client_secret, response_type="code", state="someValue"):
-        self._client_id     = client_id
-        self._client_secret = client_secret
-        self._redirect_uri  = redirect_uri
-        self._response_type = response_type
-        self._state         = state
-
-        self.state = "logout"
-
-        params = {
-            "client_id": self._client_id,
-            "redirect_uri": self._redirect_uri,
-            "response_type": self._response_type,
-            "state": self._state
-            }
-
-        self.URL["login"] += "?" + urlencode(params)
-
-
-
-    def login(self, id, pw):
-        self._id = id
-        self._pw = pw
-
-        driver.get(self.URL["login"])
-
-        #time.sleep(1)
-        driver.find_element_by_id("loginId").send_keys(self._id)
-        driver.find_element_by_id("loginPw").send_keys(self._pw)
-        driver.find_element_by_class_name("btn_login").click()
-        #time.sleep(1)
-
-        try:
-            driver.find_element_by_class_name("confirm").click()
-        except :
-            driver.quit()
-            print("아이디 또는 비밀번호가 틀렸습니다.")
-            return False
-
-        #time.sleep(1)
-
-        url = driver.current_url
-        parsed = urlparse.urlparse(url)
-        self._code = str(urlparse.parse_qs(parsed.query)["code"][0])
-
-        driver.quit()
-
-        params = {
-            "client_id": self._client_id,
-            "client_secret": self._client_secret,
-            "redirect_uri": self._redirect_uri,
-            "code": self._code,
-            "grant_type": "authorization_code"
-        }
-
-        self.URL["access"] += "?" + urlencode(params, 'utf-8')
-
-        self._token = urlopen(self.URL["access"]).read().decode('utf-8')
-
-        self.state = "login"
-        return True
-
-    def isLogin(self):
-        return self.state == "login"
-
-    def info(self):
-        if not self.isLogin():
-            print("로그인상태가 아닙니다.")
-            return False
-
-        return json.loads(urlopen(self.URL["info"] + "?" + self._token + "&output=json").read().decode())
-
-    def categoryList(self):
-        if not self.isLogin():
-            print("로그인상태가 아닙니다.")
-            return False
-        return json.loads(urlopen(self.URL["category"] + "?" + self._token + "&blogName=boycoding&output=json").read().decode())
-
-    def post(self, mode=""):
-        if not self.isLogin():
-            print("로그인상태가 아닙니다.")
-            return False
-
-        data = json.loads(urlopen(self.URL["post.list"] + "?" + self._token + "&blogName=boycoding&output=json&count=30").read().decode())
-
-        return data["tistory"]["item"]
-
     URL = \
         {
             "login": "https://www.tistory.com/oauth/authorize",
@@ -117,15 +29,146 @@ class Tistory:
             "category": "https://www.tistory.com/apis/category/list",
 
             "comment.list": "https://www.tistory.com/apis/comment/list",
-            "comment.write": "https://www.tistory.com/apis/comment/lis",
-            "comment.modify": "https://www.tistory.com/apis/comment/lis",
-            "comment.newest": "https://www.tistory.com/apis/comment/lis",
-            "comment.delete": "https://www.tistory.com/apis/comment/lis",
+            "comment.write": "https://www.tistory.com/apis/comment/write",
+            "comment.modify": "https://www.tistory.com/apis/comment/modify",
+            "comment.newest": "https://www.tistory.com/apis/comment/newest",
+            "comment.delete": "https://www.tistory.com/apis/comment/delete",
 
             "guestbook.write": "https://www.tistory.com/apis/guestbook/write",
-            "guestbook.modify": "https://www.tistory.com/apis/guestbook/write",
-            "guestbook.list": "https://www.tistory.com/apis/guestbook/write",
-            "guestbook.delete": "https://www.tistory.com/apis/guestbook/write",
+            "guestbook.modify": "https://www.tistory.com/apis/guestbook/modify",
+            "guestbook.list": "https://www.tistory.com/apis/guestbook/list",
+            "guestbook.delete": "https://www.tistory.com/apis/guestbook/delete",
         }
+
+
+    def __init__(self, client_id, redirect_uri, client_secret, response_type="code", state="someValue"):
+        self._client_id     = client_id
+        self._client_secret = client_secret
+        self._redirect_uri  = redirect_uri
+        self._response_type = response_type
+        self._state         = state
+
+        self.state = "logout"
+        self._blogName = "boycoding"
+        params = \
+            {
+            "client_id": self._client_id,
+            "redirect_uri": self._redirect_uri,
+            "response_type": self._response_type,
+            "state": self._state
+            }
+
+        self.URL["login"] += "?" + urlencode(params)
+
+
+    def login(self, id, pw):
+        self._id = id
+        self._pw = pw
+
+        driver.get(self.URL["login"])
+
+        driver.find_element_by_id("loginId").send_keys(self._id)
+        driver.find_element_by_id("loginPw").send_keys(self._pw)
+        driver.find_element_by_class_name("btn_login").click()
+
+        try:
+            driver.find_element_by_class_name("confirm").click()
+        except :
+            driver.quit()
+            print("아이디 또는 비밀번호가 틀렸습니다.")
+            return False
+
+
+        url = driver.current_url
+        parsed = urlparse.urlparse(url)
+        self._code = str(urlparse.parse_qs(parsed.query)["code"][0])
+
+        driver.quit()
+
+        params = \
+            {
+            "client_id": self._client_id,
+            "client_secret": self._client_secret,
+            "redirect_uri": self._redirect_uri,
+            "code": self._code,
+            "grant_type": "authorization_code"
+            }
+
+        self.URL["access"] += "?" + urlencode(params, 'utf-8')
+
+        self._token = urlopen(self.URL["access"]).read().decode('utf-8')
+
+        self.state = "login"
+
+        return True
+
+    def isLogin(self):
+
+        return self.state == "login"
+
+
+    def info(self):
+
+        if not self.isLogin():
+            print("로그인상태가 아닙니다.")
+            return False
+
+        data = urlopen(self.URL["info"] + "?output=json&"  + self._token).read().decode()
+
+        return self._toJsonItem(data)
+
+
+    def categoryList(self):
+
+        if not self.isLogin():
+            print("로그인상태가 아닙니다.")
+            return False
+
+        params = urlencode({"blogName":self._blogName}, "utf-8")
+
+        data = urlopen(self.URL["category"] + "?" + params + "&output=json&" + self._token).read().decode()
+
+        return self._toJsonItem(data)
+
+
+    def post_list(self, page=1, count=30, categoryId="", sort="id"):
+
+        if not self.isLogin():
+            print("로그인상태가 아닙니다.")
+            return False
+
+        params = urlencode({"page": page, "count": count, "categoryId": categoryId, "sort": sort, "blogName":self._blogName}, "utf-8")
+
+        data = urlopen(self.URL["post.list"] + "?" + params + "&output=json&" + self._token).read().decode()
+
+        return self._toJsonItem(data)
+
+
+    def post_read(self, postId):
+
+        if not self.isLogin():
+            print("로그인상태가 아닙니다.")
+            return False
+
+        params = urlencode({"postId": postId, "blogName":self._blogName}, "utf-8")
+
+        data = urlopen(self.URL["post.read"] + "?" + params + "&output=json&" + self._token).read().decode()
+
+        return self._toJsonItem(data)
+
+
+    def guestBook_list(self):
+
+        if not self.isLogin():
+            print("로그인상태가 아닙니다.")
+            return False
+
+        data = urlopen(self.URL["guestbook.list"] + "?output=json&" + self._token).read().decode()
+
+        return self._toJsonItem(data)
+
+    def _toJsonItem(self, data):
+        return json.loads(data)["tistory"]["item"]
+
 
 
